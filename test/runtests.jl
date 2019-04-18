fcf8802d087d28eddf1f442f3048e6059639805a
Eric Ford <eford@psu.edu>
2019-04-18 08:58:46 -0400

    Fixed CORBITS test

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ function run_constructor_tests()
   ExoplanetsSysSim.test_planet_constructors(sim_param)
   ExoplanetsSysSim.test_star_constructors(sim_param)
   ExoplanetsSysSim.test_planetary_system_constructors(sim_param)
-  ExoplanetsSysSim.test_corbits()
+
   ExoplanetsSysSim.test_target(sim_param)
   ExoplanetsSysSim.test_transit_observations(sim_param)
   (cat_phys, cat_obs) = ExoplanetsSysSim.test_catalog_constructors(sim_param)
@@ -21,5 +21,8 @@ function run_constructor_tests()
   ExoplanetsSysSim.test_abc_distance(cat_obs, cat_phys, sim_param)
   return 0
 end
-
 @test run_constructor_tests() == 0  # Just tests that the basic elements compile and run  # TODO: Write tests that will be useful in diagnosing any bugs
+
+#Test CORBITS moved from ExoplanetsSysSim.test_corbits() to
+using CORBITS
+include(joinpath(dirname(pathof(CORBITS)),"..","test","runtests.jl"))
